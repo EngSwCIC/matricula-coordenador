d12780624c68b86132b39768e41eac3214c30936
Lucas Moutinho <lucas.silvamoutinho@gmail.com>
2018-10-30 20:28:14 -0300

    card #61: criado metodo new para coordenadores

--- app/controllers/admins/coordinators_controller.rb
+++ app/controllers/admins/coordinators_controller.rb
@@ -1,8 +1,28 @@
 class Admins::CoordinatorsController < AdminsController
-
+  before_action :set_coordinator, only: [:destroy]
   # GET /coordinators
   # GET /coordinators.json
   def index
+    @coordinators = User.with_role(:coordinator) 
+  end
+
+  def new
+    @coordinator = User.new
+  end
+
+  def create
+    @coordinator = User.new(coordinator_params)
+
+    respond_to do |format|
+      if @coordinator.save
+        @coordinator.add_role :coordinator
+        format.html { redirect_to admin_coordinators_path, notice: 'coordinator was successfully created.' }
+        format.json { render :show, status: :created, location: @coordinator }
+      else
+        format.html { render :new }
+        format.json { render json: @coordinator.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
@@ -13,6 +33,6 @@ class Admins::CoordinatorsController < AdminsController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def coordinator_params
-      params.require(:coordinator).permit(:name, :email, :password, :admin)
+      params.require(:coordinator).permit(:name, :email, :password)
     end
 end
