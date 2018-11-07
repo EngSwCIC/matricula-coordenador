696476a35840c01c2d76f5904e948778a29543d1
igorfg <igorfgds@gmail.com>
2018-11-07 13:27:27 -0200

    rspec adicionado ao projeto

---
+++ spec/controllers/credit_forms_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe CreditFormsController, type: :controller do
+
+  describe "Test methods of CreditForms Controller"
+
+  it 'should create a new credit form' do
+    #
+  end
+
+
+end
