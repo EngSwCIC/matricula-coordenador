e08595d685af64223d7ea891f6397862164d4760
igorfg <igorfgds@gmail.com>
2018-11-07 20:31:20 -0200

    #40 Inicio dos testes da controller dos formulários de aproveitamento de crédito

--- spec/controllers/credit_forms_controller_spec.rb
+++ spec/controllers/credit_forms_controller_spec.rb
@@ -1,12 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe CreditFormsController, type: :controller do
-
-  describe "Test methods of CreditForms Controller"
-
-  it 'should create a new credit form' do
-    #
+  
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
   end
 
+  describe 'Credit forms new and create methods' do
+    it 'should return new view' do
+      get :new
+      expect(response).to have_http_status(200)
+    end
 
+    it 'should create a new credit form' do
+
+      post :create, params:{ credit_form: {name: 'joao silva', email: 'joaosilva@unb.br',
+        credit_items: [{ description: 'oi' }] }}
+      expect(flash[:success]).to eq('Seu formulário de aproveitamento de créditos foi criado com sucesso')
+      expect(CreditForm.count).to be(1)
+    end
+  end
 end
