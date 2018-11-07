81549c2415ce2c68ca1e053b8edc5eb0835cf328
Khalil <khalilcarsten@gmail.com>
2018-11-07 15:29:00 -0200

    card #61: edit and delete coordinators works

--- app/controllers/admins/coordinators_controller.rb
+++ app/controllers/admins/coordinators_controller.rb
@@ -1,5 +1,5 @@
 class Admins::CoordinatorsController < AdminsController
-  before_action :set_coordinator, only: [:destroy]
+  before_action :set_coordinator, only: [:destroy, :edit, :update]
   # GET /coordinators
   # GET /coordinators.json
   def index
@@ -25,14 +25,39 @@ class Admins::CoordinatorsController < AdminsController
     end
   end
 
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @coordinator.update_without_password(coordinator_params)
+        format.html { redirect_to admins_coordinators_path, notice: 'Coordinator was successfully updated.' }
+        format.json { render :show, status: :ok, location: @coordinator }
+      else
+        format.html { render :edit }
+        format.json { render json: @coordinator.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /coordinators/1
+  # DELETE /coordinators/1.json
+  def destroy
+    @coordinator.destroy
+    respond_to do |format|
+      format.html { redirect_to admins_coordinators_url, notice: 'Coordinator was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_coordinator
-      @coordinator = coordinator.find(params[:id])
+      @coordinator = User.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def coordinator_params
-      params.require(:user).permit(:email, :password)
+      params.require(:user).permit(:name, :email, :password)
     end
 end
