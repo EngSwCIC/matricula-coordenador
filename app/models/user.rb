56fa89bfde560680e02b764f8b07dcae68054bce
Lucas Moutinho <lucas.silvamoutinho@gmail.com>
2018-11-21 16:42:36 -0200

    model e seed de cursos: card #53

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_and_belongs_to_many :courses
+
+  # validates :courses, presence: true
+
 end
