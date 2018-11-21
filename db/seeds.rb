a0e5c6bc3027cc02daf8e16f13641fa6f047632a
Lucas Moutinho <lucas.silvamoutinho@gmail.com>
2018-11-21 05:44:37 -0200

    Model e Seed para Descrição do sistema: card #52

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,24 @@
 if !Rails.env.production?
   User.destroy_all
   puts "Usuarios apagados"
+  About.destroy_all
+  puts "Descrição apagada"
+
+
   u = User.create(email:"email@email.com", password:"aaa123")
   u.add_role(:admin)
   puts "Usuario email@email.com criado"
+
+  About.create(
+    description:"
+      Este é um projeto que visa a automação do processo de matrícula feito pelo coordenador para as disciplinas que precisam de um requerimento de matrícula.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.
+    "
+  )
+  puts "Descrição Criada"
+
 end
