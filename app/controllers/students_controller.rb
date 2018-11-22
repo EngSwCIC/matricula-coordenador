77180719ec28a2fa79f6bef9d359f023d3de891a
gabriel <gabriel>
2018-11-21 22:48:13 -0200

    card #79: simples show estudante

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -50,6 +50,10 @@ class StudentsController < ApplicationController
     end
   end
 
+  def show
+    @student = User.find(params[:id])
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_student
