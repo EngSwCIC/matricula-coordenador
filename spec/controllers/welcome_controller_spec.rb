d5196f550d93fa3e662a4d4cab9b9bf4ce2300eb
Lucas Moutinho <lucas.silvamoutinho@gmail.com>
2018-11-22 04:34:40 -0200

    rspec do welcome coordinator e do show de coordinator: card #53

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe WelcomeController, type: :controller do
 	      get :coordinators
 	      expect(response).to be_successful
 	      expect(response).to have_http_status(200)
-	    end
+			end
 
 	    it 'should render contact page' do
 	      get :contact
@@ -29,5 +29,59 @@ RSpec.describe WelcomeController, type: :controller do
 	      expect(response).to have_http_status(200)
 	    end
 	end
-	
+
+	describe "GET #index" do
+		it "return all coodinators" do
+			coor1 = User.create!(email: "coor1@coor1.com", password: "123456")
+			coor1.add_role(:coordinator)
+			coor2 = User.create!(email: "coor2@coor2.com", password: "123456")
+			coor2.add_role(:coordinator)
+			get :coordinators
+			expect(assigns(:coordinators)).to match_array([coor1, coor2])
+		end
+		
+		it "renders the :coordinators view" do
+			get :coordinators
+			expect(response).to render_template("coordinators")
+		end
+
+		it "return the description of the system in the index" do
+			about = About.create(description:"Bom dia")
+			get :index
+			expect(assigns(:about)).to match(about)
+		end
+		
+		it "renders the :index view" do
+			get :index
+			expect(response).to render_template("index")
+		end
+
+		it "return the description of the system in the about" do
+			about = About.create(description:"Bom dia")
+			get :about
+			expect(assigns(:about)).to match(about)
+		end
+		
+		it "renders the :about view" do
+			get :about
+			expect(response).to render_template("about")
+		end
+
+	end
+
+	describe "GET #show" do
+		before(:each) do 
+			@coordinator = User.create!(email: "coor1@coor1.com", password: "123456")
+			@coordinator.add_role(:coordinator)
+			get :show, params: {id: @coordinator.id}
+		end
+
+		it "return the coordinator" do
+			expect(assigns(:coordinator)).to match(@coordinator)
+		end
+
+		it "renders the :show template" do
+			expect(response).to render_template(:show)
+		end
+	end
 end
