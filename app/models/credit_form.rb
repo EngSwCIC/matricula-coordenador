6e78a264010eb818c145862de876843ebdf9cbec
Lucas Moutinho <lucas.silvamoutinho@gmail.com>
2018-11-22 00:19:24 -0200

    consertado bug de relacao: card #63

--- app/models/credit_form.rb
+++ app/models/credit_form.rb
@@ -1,6 +1,6 @@
 class CreditForm < ApplicationRecord
   ITEMS_COUNT_MIN = 1
-  belongs_to :user, optional: true
+  belongs_to :user
 
   has_many :credit_items, :dependent => :destroy, index_errors: true
   accepts_nested_attributes_for :credit_items, allow_destroy: true
