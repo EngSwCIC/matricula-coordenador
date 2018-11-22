61962d065d19d4dcf0a8fd50a9d68bea5ca877e4
arturhbs <arturhenrique71@gmail.com>
2018-11-22 01:59:12 -0200

    sem pendencias

--- spec/helpers/admins/abouts_helper_spec.rb
+++ spec/helpers/admins/abouts_helper_spec.rb
@@ -11,7 +11,7 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe Admins::AboutsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
 end  
 RSpec.describe Admins::StudentsHelper, type: :helper do
   # pending "add some examples to (or delete) #{__FILE__}"
