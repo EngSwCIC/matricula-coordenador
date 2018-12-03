66787e9e9469faa74a7dc5172d0553fc47a369a7
igorfg <igorfgds@gmail.com>
2018-12-03 20:20:47 -0200

    D

---
+++ features/step_definitions/formulario/criar_formulario_steps.rb
@@ -0,0 +1,25 @@
+Dado("que eu seja um estudante") do
+  user = User.create(name: "student", email: "email@email.com", password: "aaa123")
+  user.add_role(:student)
+end
+
+Quando("preencher {string} com {string}") do |string, string2|
+  fill_in string, with: string2
+end
+
+Quando("anexar {string} com {string}") do |string, string2|
+  attach_file string, Rails.root.join('public', string2)
+end
+
+
+Dado("esteja na pagina para criar formularios") do
+  visit credit_forms_path
+end
+
+Então("sou redirecionado para página de formularios") do
+  expect(current_path).to eql(new_credit_form_path)
+end
+
+Então("sou redirecionado para a página de novo formulario") do
+  expect(current_path).to eql(credit_forms_path)
+end
