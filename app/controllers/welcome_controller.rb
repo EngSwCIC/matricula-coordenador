308ece8262705d5f90d4799036b6f132c350a180
gabriel <gabriel>
2018-12-05 21:13:19 -0200

    card #92: ajustes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
 class WelcomeController < ApplicationController
+  #pagina estatica home com informações do sistema
   def index
     @about = About.first
   end
 
+  #pagina estatica about com descrição do funcionamento do sistema
   def about
     @about = About.first
   end
@@ -15,9 +19,12 @@ class WelcomeController < ApplicationController
     @coordinator = User.find(params[:id])
   end
 
+  #pagina estatica com informações de contato
   def contact
   end
 
+  ##pagina information com informações sobre o que é necessário pra entrar lista de espera
   def information
   end
+
 end
