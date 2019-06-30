343e6603ffe86c988e7dbd30a0f5bcf06a94c871
Lucas de Albuquerque <lks.albq@gmail.com>
2019-06-30 02:08:03 -0300

    criando testes na controller e model

--- spec/models/reminder_spec.rb
+++ spec/models/reminder_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Reminder, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation tests' do
+    before(:each) do
+      @coordinator = User.new(email: 'coordinator@coordinator.com', password: '123456')
+      @coordinator.add_role(:coordinator)
+      @coordinator.save
+    end
+
+    it 'ensures remind presence' do
+      remind = Reminder.new(remind_date: 1.day.from_now , attendance_request: AttendanceRequest.first, user: @coordinator).save
+      expect(remind).to eq(false)
+    end
+
+    it 'ensures remind date' do
+      remind = Reminder.new(remind: "Lembrar", attendance_request: AttendanceRequest.first, user: @coordinator).save
+      expect(remind).to eq(false)
+    end
+
+    it 'ensures remind date' do
+      remind = Reminder.new(remind: "Lembrar", remind_date: 1.day.from_now , attendance_request: AttendanceRequest.first, user: @coordinator).save
+      expect(remind).to eq(true)
+    end
+  end
 end
