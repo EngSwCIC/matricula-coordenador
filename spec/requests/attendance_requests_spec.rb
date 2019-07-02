65ee018fcfe892e1f77b11c82e9f5e93fd1d5ef4
Pablo Alves Muro Martinez <pablo.martinez@brisabr.com.br>
2019-07-02 18:35:12 -0300

    Init rspec tests

--- spec/requests/attendance_requests_spec.rb
+++ spec/requests/attendance_requests_spec.rb
@@ -1,10 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe "AttendanceRequests", type: :request do
+  before(:each) do
+    @course = Course.create(name: 'Ciência da Computação (Bacharel)')
+    @student = User.new(name: 'João', email: 'student@student.com', password: '123456', course: @course)
+    @student.add_role(:student)
+    @student.save
+    sign_in @student
+  end
+  
   describe "GET /attendance_requests" do
-    it "works! (now write some real specs)" do
+    it "has the text Requisição de atedimento" do
       get attendance_requests_path
       expect(response).to have_http_status(200)
+      expect(response.body).to include('Requisição de atedimento');
+    end
+  end
+
+  describe "NEW /attendance_requests" do
+    it "has the text Requisição de atedimento" do
+      get attendance_requests_path
+      expect(response).to have_http_status(200)
+      expect(response.body).to include('Requisição de atedimento');
     end
   end
 end
